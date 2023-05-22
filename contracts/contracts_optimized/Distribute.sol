c88a1f67e6d8164a51f5bcb7e1409e657bf3fc1e
Hammad Ghazi <hammadghazi007@gmail.com>
2023-05-22 17:41:16 +0500

    Distribute puzzle solved

--- contracts/contracts_optimized/Distribute.sol
+++ contracts/contracts_optimized/Distribute.sol
@@ -2,12 +2,27 @@
 pragma solidity 0.8.15;
 
 contract OptimizedDistribute {
-    address[4] payable public contributors;
-    uint256 public createTime;
+    address payable immutable contributor1;
+    address payable immutable contributor2;
+    address payable immutable contributor3;
+    address payable immutable contributor4;
+
+    uint256 public immutable createTime;
+    uint256 public immutable amount;
+
+    // Used selfdestruct to transfer eth to last contributor (saves 4k gas, send costs around 9k whereas selfdestruct around 4k)
+    // Used 4x immutable contributors var instead of a 4 length array
+    // Calculated amount in constructor and made it immutable
+    // Only setting createTime during constructor initialisation hence made it immutable as well
+    // Used send to transfer eth instead of transfer (send is cheaper as it doesn't revert if the transfer is failed)
 
     constructor(address[4] memory _contributors) payable {
-        contributors = _contributors;
+        contributor1 = payable(_contributors[0]);
+        contributor2 = payable(_contributors[1]);
+        contributor3 = payable(_contributors[2]);
+        contributor4 = payable(_contributors[3]);
         createTime = block.timestamp;
+        amount = address(this).balance / 4;
     }
 
     function distribute() external {
@@ -15,11 +30,9 @@ contract OptimizedDistribute {
             block.timestamp > createTime + 1 weeks,
             'cannot distribute yet'
         );
-
-        uint256 amount = address(this).balance / 4;
-        payable(contributors[0]).transfer(amount);
-        payable(contributors[1]).transfer(amount);
-        payable(contributors[2]).transfer(amount);
-        payable(contributors[3]).transfer(amount);
+        contributor1.send(amount);
+        contributor2.send(amount);
+        contributor3.send(amount);
+        selfdestruct(contributor4);
     }
 }
--- contracts/contracts_optimized/Mint150.sol
+++ contracts/contracts_optimized/Mint150.sol
@@ -1,7 +1,8 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.15;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import 'hardhat/console.sol';
 
 // You may not modify this contract or the openzeppelin contracts
 contract NotRareToken is ERC721 {
@@ -9,15 +10,44 @@ contract NotRareToken is ERC721 {
 
     uint256 private totalSupply;
 
-    constructor() ERC721("NotRareToken", "NRT") {}
+    constructor() ERC721('NotRareToken', 'NRT') {}
 
     function mint() external {
         totalSupply++;
+        console.log('Minter ', msg.sender);
+        console.log('Token id ', totalSupply);
         _safeMint(msg.sender, totalSupply);
         alreadyMinted[msg.sender] = true;
     }
 }
 
-contract Attacker {
-    constructor(address victim) {}
+contract OptimizedAttacker {
+    address public immutable deployer;
+    NotRareToken public immutable victim;
+
+    constructor(NotRareToken _victim) {
+        console.log('deployer ', msg.sender);
+        console.log('Attacker contract ', address(this));
+        deployer = msg.sender;
+        victim = _victim;
+        // for (uint256 i = 0; i < 10; i++) {
+        this.tryMint();
+        // victim.mint();
+        // }
+    }
+
+    function tryMint() public {
+        victim.mint();
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        console.log('Mic testing');
+        victim.safeTransferFrom(address(this), deployer, 1);
+        return IERC721Receiver.onERC721Received.selector;
+    }
 }
